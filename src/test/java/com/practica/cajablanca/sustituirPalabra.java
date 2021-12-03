9a81ab972baedbf87a6aaee5290959253491ee74
SoniaZhang9 <sonia.zhang@alumnos.upm.es>
2021-12-03 15:47:16 +0100

    System.println instructions deleted

--- src/test/java/com/practica/cajablanca/sustituirPalabra.java
+++ src/test/java/com/practica/cajablanca/sustituirPalabra.java
@@ -37,7 +37,6 @@ public class sustituirPalabra {
         // Comprueba si el contenido de la primera posicion de editor es igual a una lista vacia -> []
         assertEquals(this.editor.getLinea(1).toString(), "[]");
 
-        System.out.println(this.editor.getLinea(1).toString()); //borrar esta linea luego
     }
 
     @Test
@@ -47,30 +46,24 @@ public class sustituirPalabra {
 
         // Comprueba si el contenido de la primera posicion de editor es igual a [camion]
         assertEquals(this.editor.getLinea(1).toString(), "[camion]");
-
-        System.out.println(this.editor.getLinea(1).toString()); //borrar esta linea luego
     }
 
-            @Test
-        public void test4() throws EmptyCollectionException {
-            this.editor.leerFichero("src/test/ficheros/sustituirPalabra/fich4");
-            this.editor.sustituirPalabra("coche","camion");
-            assertEquals(this.editor.getLinea(1).toString(), "[]");
-            assertEquals(this.editor.getLinea(2).toString(), "[]");
+    @Test
+    public void test4() throws EmptyCollectionException {
+        this.editor.leerFichero("src/test/ficheros/sustituirPalabra/fich4");
+        this.editor.sustituirPalabra("coche","camion");
+        assertEquals(this.editor.getLinea(1).toString(), "[]");
+        assertEquals(this.editor.getLinea(2).toString(), "[]");
+    }
 
-            System.out.println(this.editor.getLinea(1).toString());
-            System.out.println(this.editor.getLinea(2).toString());
-        }
+    @Test
+    public void test5() throws EmptyCollectionException {
+        this.editor.leerFichero("src/test/ficheros/sustituirPalabra/fich5");
 
-        @Test
-        public void test5() throws EmptyCollectionException {
-            this.editor.leerFichero("src/test/ficheros/sustituirPalabra/fich5");
-
-            this.editor.sustituirPalabra("coche","camion");
-// Esta prueba lo hacemos para comprobar si en nuestro editor tenemos una palabra que no es coche entonces no puede sustituir
-            // y se devuelve la palabra que estaba en el editor.
-            //Comprueba si el contenido de la primera posicion de editor es igual a [bicicleta]
-            assertEquals(this.editor.getLinea(1).toString(), "[bicicleta]");
-            System.out.println(this.editor.getLinea(1).toString());
-        }
+        this.editor.sustituirPalabra("coche","camion");
+        // Esta prueba lo hacemos para comprobar si en nuestro editor tenemos una palabra que no es coche entonces no puede sustituir
+        // y se devuelve la palabra que estaba en el editor.
+        //Comprueba si el contenido de la primera posicion de editor es igual a [bicicleta]
+        assertEquals(this.editor.getLinea(1).toString(), "[bicicleta]");
+    }
 }
