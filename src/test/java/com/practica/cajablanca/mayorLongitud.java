5425df0d04e84cd4b3d79605f7064e274bc31bb0
eguadalupe <63119020+eguadalupe@users.noreply.github.com>
2021-12-11 12:55:35 +0100

    Update mayorLongitud.java
    Test mayorLong completado

--- src/test/java/com/practica/cajablanca/mayorLongitud.java
+++ src/test/java/com/practica/cajablanca/mayorLongitud.java
@@ -11,13 +11,27 @@ public class mayorlongitudTest {
     public void setUp(){
         this.editor = new Editor();
     }
+    
     @Test
-    void test1camino(){
+    void test1camino1() throws EmptyCollectionException{
         assertNull(editor.mayorLongitud());
     }
+    
     @Test
-    void test2camino() throws EmptyCollectionException {
-        editor.leerFichero("src/test/ficheros/FicherosMayorLong/Texto2");
-        assertEquals(editor.mayorLongitud(), "Pablito");
+    void test2camino4() throws EmptyCollectionException {
+        editor.leerFichero("src/test/ficheros/FicherosMayorLong/TextoTest2");
+        assertEquals("Hola", editor.mayorLongitud());
+    }
+    
+    @Test
+    void test3camino5_1() throws EmptyCollectionException {
+        editor.leerFichero("src/test/ficheros/FicherosMayorLong/TextoTest3");
+        assertEquals("hola", editor.mayorLongitud());
+    }
+    
+    @Test
+    void test4camino6_1() throws EmptyCollectionException {
+        editor.leerFichero("src/test/ficheros/FicherosMayorLong/TextoTest4");
+        assertEquals("holas", editor.mayorLongitud());
     }
 }
