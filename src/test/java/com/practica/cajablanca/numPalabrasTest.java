004b88f25907165f8b550e5fe8e07afb207902f5
eguadalupe <63119020+eguadalupe@users.noreply.github.com>
2021-12-11 14:20:39 +0100

    Update numPalabrasTest.java
    test completado

--- src/test/java/com/practica/cajablanca/numPalabrasTest.java
+++ src/test/java/com/practica/cajablanca/numPalabrasTest.java
@@ -14,50 +14,50 @@ public class numPalabrasTest {
 
     //Test para el camino 1: inicio menor o igual que cero
     @Test
-    public void test1 (){
+    public void test1Camino1 (){
         Editor editor = new Editor();
-        assertThrows(IllegalArgumentException.class, () -> {editor.numPalabras(0, 1, "hola");});
+        assertThrows(IllegalArgumentException.class, () -> {editor.numPalabras(0, 1, "coche");});
     }
 
     //Test para el camino 2: fin no puede ser mayor que el tamaño de editor
     @Test
-    public void test2 (){
+    public void test2Camino2 (){
         Editor editor = new Editor();
-        assertThrows(IllegalArgumentException.class, () -> {editor.numPalabras(1, 1, "hola");});
+        assertThrows(IllegalArgumentException.class, () -> {editor.numPalabras(1, 1, "coche");});
     }
 
     //Test para el camino 3: tamaño de editor igual a cero
     @Test
-    public void test3 (){
+    public void test3Camino3 (){
         Editor editor = new Editor();
-        assertEquals(0, editor.numPalabras(1, 0, "hola"));
+        assertEquals(0, editor.numPalabras(1, 0, "coche"));
     }
 
     //Test para el camino 4: tamaño de editor mayor que 0 e inicio = fin
     @Test
-    public void test4 (){
+    public void test4Camino4 (){
         Editor editor = new Editor();
-        editor.leerFichero("src/main/java/com/practica/cajablanca/prueba.txt");
-        assertEquals(0, editor.numPalabras(1,1, "hola"));
+        editor.leerFichero("src/test/ficheros/numPalabras/textoTest4");
+        assertEquals(0, editor.numPalabras(1,1, "coche"));
     }
 
-    //Test para el camino 5: tamaño de editor mayor que 0, inicio menor que fin, pos mayor que tamaño lista
+   
     //Camino 5 no valido
     //Test para el camino 6: tamaño de editor mayor que 0, inicio menor que fin, pos menor o igual
     //que tamaño lista, fin - inicio = 1, editor.getAtPos(inicio) != palabra
     @Test
-    public void test5y6 (){
+    public void test5Camino6 (){
         Editor editor = new Editor();
-        editor.leerFichero("src/main/java/com/practica/cajablanca/prueba.txt");
+        editor.leerFichero("src/test/ficheros/numPalabras/textoTest5");
         assertEquals(0, editor.numPalabras(1,2, "coche"));
     }
 
     //Test para el camino 7: tamaño de editor mayor que 0, inicio menor que fin, pos menor o igual
     //que tamaño lista, editor.getAtPos(inicio) == palabra
     @Test
-    public void test7 (){
+    public void test6Camino7 (){
         Editor editor = new Editor();
-        editor.leerFichero("src/main/java/com/practica/cajablanca/prueba.txt");
+        editor.leerFichero("src/test/ficheros/numPalabras/textoTest6");
         assertEquals(1, editor.numPalabras(1,2, "coche"));
     }
 }
